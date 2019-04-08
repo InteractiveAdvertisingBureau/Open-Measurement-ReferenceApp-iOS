d58cddfbdbc2b58c18ef6a265c77e2178478e60f
Alex Chugunov <achugunov@pandora.com>
2019-04-08 15:41:55 -0700

    Remove demo-server and demo creatives (they moved to AWS)

--- demo-server/serve.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-var port = 8787
-
-app.use('/', express.static(__dirname + '/'));
-
-app.listen(port, () => console.log('Listening at http://localhost:' + port))
