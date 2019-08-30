46799567b771c4b72f1c9c05fd28ddd2436adb1f
Justin Hines <jhines@pandora.com>
2019-08-30 08:37:32 -0700

    Add OMSDK native 1.3 lib to ref app

--- OM-Demo/BridgingHeader.h
+++ OM-Demo/BridgingHeader.h
@@ -6,6 +6,8 @@
 //  Copyright © 2019 Open Measurement Working Group. All rights reserved.
 //
 
+// TODO: Remove this file once lib is built as a framework
+
 #ifndef BridgingHeader_h
 #define BridgingHeader_h
 
