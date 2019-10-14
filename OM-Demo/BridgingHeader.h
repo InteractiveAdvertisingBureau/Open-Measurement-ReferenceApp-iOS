5a80493769d97502dce1ba5775532765c97ae132
Justin Hines <jhines@pandora.com>
2019-10-14 16:28:07 -0700

    Remove unnecessary import files

---
+++ OM-Demo/BridgingHeader.h
@@ -0,0 +1,18 @@
+//
+//  BridgingHeader.h
+//  OM-Demo
+//
+//  Created by Justin Hines on 8/15/19.
+//  Copyright © 2019 Open Measurement Working Group. All rights reserved.
+//
+
+// TODO: Remove this file once lib is built as a framework
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import "OMIDImports.h"
+
+#endif /* BridgingHeader_h */
+
+
