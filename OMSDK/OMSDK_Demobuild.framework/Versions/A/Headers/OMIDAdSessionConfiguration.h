4c20840b292c6935a495b42e80b06cb228187576
juo007 <juo007@ucsd.edu>
2019-11-21 14:08:29 -0800

    Update reference app

--- OMSDK/OMSDK_Demobuild.framework/Versions/A/Headers/OMIDAdSessionConfiguration.h
+++ OMSDK/OMSDK_Demobuild.framework/Versions/A/Headers/OMIDAdSessionConfiguration.h
@@ -7,28 +7,83 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ * Identifies which integration layer is responsible for sending certain events.
+ */
 typedef NS_ENUM(NSUInteger, OMIDOwner) {
-    OMIDJavaScriptOwner = 1, // will translate into "JAVASCRIPT" when published to the OMID JS service.
-    OMIDNativeOwner = 2, // will translate into "NATIVE" when published to the OMID JS service.
-    OMIDNoneOwner = 3 // will translate into "NONE" when published to the OMID JS service.
+    /** The integration will send the event from a JavaScript session script. */
+    OMIDJavaScriptOwner = 1,
+    /** The integration will send the event from the native layer. */
+    OMIDNativeOwner = 2,
+    /** The integration will not send the event. */
+    OMIDNoneOwner = 3
 };
 
-@interface OMIDDemobuildAdSessionConfiguration : NSObject
 
+/**
+ *  List of supported creative types
+ */
+typedef NS_ENUM(NSUInteger, OMIDCreativeType) {
+    // Creative type will be set by JavaScript session script.
+    // Integrations must also pass Owner.JAVASCRIPT for impressionOwner.
+    OMIDCreativeTypeDefinedByJavaScript = 1,
+    // Remaining values set creative type in native layer.
+    OMIDCreativeTypeHtmlDisplay = 2,
+    OMIDCreativeTypeNativeDisplay = 3,
+    OMIDCreativeTypeVideo = 4,
+    OMIDCreativeTypeAudio = 5
+};
+
+/**
+ * List of supported impression types
+ */
+typedef NS_ENUM(NSUInteger, OMIDImpressionType) {
+  // ImpressionType will be set by JavaScript session script.
+  // Integrations must also pass Owner.JAVASCRIPT for impressionOwner.
+  OMIDImpressionTypeDefinedByJavaScript = 1,
+  // Remaining values set ImpressionType in native layer.
+  OMIDImpressionTypeUnspecified = 2,
+  OMIDImpressionTypeLoaded = 3,
+  OMIDImpressionTypeBeginToRender = 4,
+  OMIDImpressionTypeOnePixel = 5,
+  OMIDImpressionTypeViewable = 6,
+  OMIDImpressionTypeAudible = 7,
+  OMIDImpressionTypeOther = 8
+};
+
+/**
+ * The ad session configuration supplies the owner for both the impression and video events.
+ * The OMID JS service will use this information to help identify where the source of these
+ * events is expected to be received.
+ */
+@interface OMIDAdSessionConfiguration : NSObject
+
+@property OMIDCreativeType creativeType;
+@property OMIDImpressionType impressionType;
 @property OMIDOwner impressionOwner;
-@property OMIDOwner videoEventsOwner;
+@property OMIDOwner mediaEventsOwner;
 @property BOOL isolateVerificationScripts;
 
-/// Returns nil and sets error if OMID isn't active or arguments are invalid.
-/// @param impressionOwner providing details of who is responsible for triggering the impression event.
-/// @param videoEventsOwner providing details of who is responsible for triggering video events. This is only required for video ad sessions and should be set to videoEventsOwner:OMIDNoneOwner for display ad sessions.
-/// @param isolateVerificationScripts determines whether verification scripts will be placed in a sandboxed environment. This will not have any effect for native sessions.
+- (nullable instancetype)initWithCreativeType:(OMIDCreativeType)creativeType
+                               impressionType:(OMIDImpressionType)impressionType
+                              impressionOwner:(OMIDOwner)impressionOwner
+                             mediaEventsOwner:(OMIDOwner)mediaEventsOwner
+                   isolateVerificationScripts:(BOOL)isolateVerificationScripts
+                                        error:(NSError *_Nullable *_Nullable)error;
+
+#pragma mark - Deprecated Methods
+
+/**
+ * Returns nil and sets error if OMID isn't active or arguments are invalid.
+ * Note: Planned to be deprecated in OM SDK 1.3.2.
+ * @param impressionOwner providing details of who is responsible for triggering the impression event.
+ * @param videoEventsOwner providing details of who is responsible for triggering video events. This is only required for video ad sessions and should be set to videoEventsOwner:OMIDNoneOwner for display ad sessions.
+ * @param isolateVerificationScripts determines whether verification scripts will be placed in a sandboxed environment. This will not have any effect for native sessions.
+ */
 - (nullable instancetype)initWithImpressionOwner:(OMIDOwner)impressionOwner
                                 videoEventsOwner:(OMIDOwner)videoEventsOwner
                       isolateVerificationScripts:(BOOL)isolateVerificationScripts
                                            error:(NSError *_Nullable *_Nullable)error;
-
-#pragma mark - Deprecated Methods
 
 - (nullable instancetype)initWithImpressionOwner:(OMIDOwner)impressionOwner
                                 videoEventsOwner:(OMIDOwner)videoEventsOwner
--- OMSDK/OMSDK_Demobuild.framework/Versions/A/Headers/OMIDSDK.h
+++ OMSDK/OMSDK_Demobuild.framework/Versions/A/Headers/OMIDSDK.h
@@ -9,56 +9,66 @@
 
 /// API Note: this value must be copied into the ad SDK's binary. It cannot be an extern defined in
 /// the OMID library.
-#define OMIDSDKAPIVersionString @"{\"v\":\"1.2.13\",\"a\":\"1\"}"
+#define OMIDSDKAPIVersionString @"{\"v\":\"1.2.20\",\"a\":\"1\"}"
 
-/*!
- * @discussion This application level class will be called by all integration partners to ensure OM SDK has been activated before calling any other API methods.
+/**
+ *  This application level class will be called by all integration partners to ensure OM SDK has been activated before calling any other API methods.
  * Any attempt to use other API methods prior to activation will result in an error.
  *
  * Note that OM SDK may only be used on the main UI thread.
  * Make sure you are on the main thread when you initialize the SDK, create its
  * objects, and invoke its methods.
  */
-@interface OMIDDemobuildSDK : NSObject
+@interface OMIDSDK : NSObject
 
-/*!
- * @abstract The current semantic version of the integrated OMID library.
+/**
+ *  The current semantic version of the integrated OMID library.
  */
 + (nonnull NSString *)versionString;
 
-/*!
- * @abstract Allows the integration partner to check that they are compatible with the running OMID library version.
+/**
+ *  Shared OMIDSDK instance.
+ */
+@property(class, readonly, nonnull) OMIDSDK *sharedInstance
+NS_SWIFT_NAME(shared);
+
+/**
+ *  A Boolean value indicating whether the OMID library has been activated.
+ *
+ *  The value of this property is YES if the OMID library has already been activated. Allows the integration partner to check that they are compatible with the running OMID library version.
+ */
+@property(atomic, readonly, getter = isActive) BOOL active;
+
+/**
+ *  Enables the integration partner to activate OMID.
+ */
+
+- (BOOL)activate;
+
+#pragma mark - Deprecated Methods
+
+/**
+ *  Allows the integration partner to check that they are compatible with the running OMID library version.
  *
  * @param OMIDAPIVersion The version of OMID library integrated by the partner.
  * @return YES if the version supplied is compatible with the integrated OMID library version.
  *
- * Note: Planned to be deprecated in next major release
+ * Note: Planned to be deprecated in OM SDK 1.3.2.
  */
 
 + (BOOL)isCompatibleWithOMIDAPIVersion:(nonnull NSString *)OMIDAPIVersion
 NS_SWIFT_NAME(isCompatible(withOMIDAPIVersion:));
 
-/*!
- * @abstract Shared OMIDSDK instance.
- */
-@property(class, readonly, nonnull) OMIDDemobuildSDK *sharedInstance
-NS_SWIFT_NAME(shared);
-
-/*!
- * @abstract A Boolean value indicating whether the OMID library has been activated.
- *
- * @discussion The value of this property is YES if the OMID library has already been activated. Allows the integration partner to check that they are compatible with the running OMID library version.
- */
-@property(atomic, readonly, getter = isActive) BOOL active;
-
-/*!
- * @abstract Enables the integration partner to activate OMID prior to calling any other API methods.
+/**
+ *  Enables the integration partner to activate OMID prior to calling any other API methods.
  *
  * @param OMIDAPIVersion The version of OMID library integrated by the partner.
  * @param error If an error occurs, contains an NSError object that describes the problem.
  * @return YES if activation was successful when checking the supplied version number for compatibility.
  *
+ * Note: Planned to be deprecated in OM SDK 1.3.2.
  */
+
 - (BOOL)activateWithOMIDAPIVersion:(nonnull NSString *)OMIDAPIVersion
                              error:(NSError *_Nullable *_Nullable)error;
 
